347b98b82d54b9ca6b67b95926ae22c3dc8730cd
Puginator5000 <emmaminime3662@gmail.com>
2024-10-13 14:20:45 -0700

    Done

--- Main.java
+++ Main.java
@@ -4,6 +4,38 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //problem1();
+    problem2();
+  }
+
+  private static void problem1() {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the raidus of the circle: ");
+    double input = sc.nextDouble();
+
+    Circle circ = new Circle(input);
+
+    //System.out.println("A circle with a radius of " + input + " has a circumerence of " circ.getCircumference() + " and an area of " + circ.getArea());
+
+    System.out.println("A A circle with a radius of" + input + " has a circumference of " + circ.getCircumference() + " and an area of " + circ.getArea());
+
+  }
+
+  private static void problem2() {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number of sides: ");
+    int input1 = sc.nextInt();
+
+    System.out.println("Enter the side length: ");
+    double input2 = sc.nextDouble();
+
+    RegularPolygon shape = new RegularPolygon(input1, input2);
+
+    System.out.println("Area with " + shape.getNumSides() + " sides: " + shape.getArea());
+    
+    shape.addSides(2);
+    System.out.println("Area with " + shape.getNumSides() + " sides: " + shape.getArea());
   }
 }
